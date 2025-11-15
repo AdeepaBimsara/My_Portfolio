4da5ca42d2ffc1b86438034892471f5b97f90b04
Adeepa_Bimsara <adeepabimsara6@gmail.com>
2025-11-15 10:35:08 +0530

    create all

---
+++ js/index.js
@@ -0,0 +1,15 @@
+   <!--========Hero Section js==============-->
+
+    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+    <script>
+        var typed = new Typed('.auto-type', {
+            strings: ["Frontend Developer", "Backend Developer", "Software Engineer", "Java Developer"],
+            typeSpeed: 100,
+            backSpeed: 50,
+            loop: true
+        });
+    </script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
